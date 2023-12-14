9625d806df26515e977d35481349e6748cdf81dd
Muizzuddin Arif <muizzuddin291@gmail.com>
2023-12-14 12:28:02 +0800

    delete working

--- collegeRegistration/admin/staff1.php
+++ collegeRegistration/admin/staff1.php
@@ -12,8 +12,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Performing SQL query to fetch data from the student table
-$sql = "SELECT * FROM student";
+// Performing SQL query to fetch data from the student table with status 'enable'
+$sql = "SELECT * FROM student WHERE status = 'enable'";
 $result = $conn->query($sql);
 
 // Checking if there are rows returned
@@ -22,13 +22,14 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["ID"] . "</td>";
-        echo "<td>" . $row["fullName"] . "</td>";
+        echo "<td><a href='#' class='edit-btn' data-id='" . $row["ID"] . "'>" . $row["fullName"] . "</a></td>";
         echo "<td>" . $row["email"] . "</td>";
         echo "<td>" . $row["phoneNumber"] . "</td>";
         echo "<td>" . $row["programCode"] . "</td>";
         echo "<td>" . $row["semester"] . "</td>";
         echo "<td>" . $row["college"] . "</td>";
         echo "<td>" . $row["roomNumber"] . "</td>";
+        echo "<td><a href='#' onclick='confirmDelete(" . $row["ID"] . ")'>Delete</a></td>";
         echo "</tr>";
     }
 } else {
